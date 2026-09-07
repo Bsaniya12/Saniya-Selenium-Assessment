919b80daba0f4bab90f442195a848a2acacaf160
Bsaniya12 <167527659+Bsaniya12@users.noreply.github.com>
2026-09-07 21:44:11 +0530

    Add files via upload

---
+++ ScreenshotTestCase.java
@@ -0,0 +1,37 @@
+package assessments;
+
+import java.io.File;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+
+public class ScreenshotTestCase 
+{
+	public static void main(String[] args) throws Exception {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        
+        JavascriptExecutor js=(JavascriptExecutor)driver;
+		js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+		
+		TakesScreenshot tks = (TakesScreenshot) driver;
+		
+		File src = tks.getScreenshotAs(OutputType.FILE);
+		File dest = new File("./screenshots/products-page.png");
+		dest.getParentFile().mkdirs();
+		FileHandler.copy(src, dest);
+		Thread.sleep(3000);
+		
+		driver.quit();
+}
+}
